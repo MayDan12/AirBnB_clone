8384534fa325115c3859f047431c834f2cee8dc3
Karl <tekamkarl@gmail.com>
2023-12-09 20:50:37 +0100

    storage in a file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import json
+"""
+this moduleis responsible for serializing instances to a JSON file
+and deserializing a JSON file to instances.
+"""
+
+class FileStorage:
+    """
+    that serializes instances to a JSON file 
+    and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """  sets in __objects the obj with key <obj class name>.id """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file """
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists """
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_data in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = globals()[class_name](**obj_data)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            print("File not found")
