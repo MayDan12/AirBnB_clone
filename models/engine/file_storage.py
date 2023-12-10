5cb6a46c7fda08303d2ae85b51ec569e4972dd9f
Karl <tekamkarl@gmail.com>
2023-12-10 22:55:50 +0100

    some ajustment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This function Defines the FileStorage class."""
 import json
+from models.base_model import BaseModel
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
 
 
 class FileStorage:
